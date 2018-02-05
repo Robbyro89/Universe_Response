f657195225e9badd2cb33a4ae02ee8f6983e574a
Robbyro89 <gallery.laroche.id@gmail.com>
2018-02-06 00:12:30 +0800

    edit controler and views

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,2 +1,18 @@
 class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def secret
+    unless logged_in?
+      redirect_to login_path
+      flash[:danger] = "Veuillez entrer vos identifiants"
+    end
+  end
+
+  def about
+  end
+
+  def contact
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
-  get 'users/index'
 
-  get 'users/new'
+  resources :users
 
-  get 'users/show'
+  root "static_pages#home"
 
-  get 'users/edit'
+  get '/secret',    to: 'static_pages#secret'
+  get '/about',     to: 'static_pages#about'
+  get '/contact',   to: 'static_pages#about'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/signup',    to: 'users#new'
+
+  get '/login',     to: 'sessions#new'
+  post '/login',    to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
 end
